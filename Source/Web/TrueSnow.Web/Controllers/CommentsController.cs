a3e0c30500778ac4b07d9ae315d7e4729ef30db8
BiserSirakov <biser.sirakov@gmail.com>
2016-02-20 00:44:53 +0200

    SearchController - for users

--- Source/Web/TrueSnow.Web/Controllers/CommentsController.cs
+++ Source/Web/TrueSnow.Web/Controllers/CommentsController.cs
@@ -15,10 +15,12 @@
     {
         private const int CommentsPerPage = 5;
         private readonly ICommentsService comments;
+        private readonly UserManager<User> userManager;
 
-        public CommentsController(ICommentsService comments)
+        public CommentsController(ICommentsService comments, UserManager<User> userManager)
         {
             this.comments = comments;
+            this.userManager = userManager;
         }
 
         public ActionResult ByPost(int id)
@@ -40,11 +42,16 @@
                 var commentToAdd = new Comment
                 {
                     Content = comment.Content,
-                    CreatorId = this.HttpContext.User.Identity.GetUserId(),
+                    CreatorId = this.User.Identity.GetUserId(),
                     PostId = comment.PostId
                 };
 
                 this.comments.Add(commentToAdd);
+
+                var currentUser = this.userManager.FindById(this.User.Identity.GetUserId());
+                var creatorAvatarId = currentUser.Files.First(x => x.FileType == FileType.Avatar).Id;
+                var createdOnStr = commentToAdd.CreatedOn.ToString();
+                return this.Json(new { Content = commentToAdd.Content, CreatorAvatarId = creatorAvatarId, CreatedOn = createdOnStr });
             }
 
             return this.Redirect(this.Request.UrlReferrer.ToString());
---
+++ Source/Web/TrueSnow.Web/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿namespace TrueSnow.Web.Controllers
+{
+    using System.Linq;
+    using Data.Models;
+    using Microsoft.AspNet.Identity;
+    using System.Web.Mvc;
+    using Infrastructure.Mapping;
+    using Models.Users;
+    public class SearchController : BaseController
+    {
+        private readonly UserManager<User> userManager;
+
+        public SearchController(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public ActionResult GetSearch()
+        {
+            return this.PartialView("GetSearch");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Search(string query)
+        {
+            var model = this.userManager
+                .Users
+                .Where(x => x.FirstName.ToLower().Contains(query.ToLower()) || x.LastName.ToLower().Contains(query.ToLower()))
+                .To<ProfileViewModel>()
+                .ToList();
+
+            return this.View(model);
+        }
+    }
+}
